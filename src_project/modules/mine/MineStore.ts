134bbec506b6c3f2c01c4db666d0e3658e81b2df
BlendAndroid <1059152860@qq.com>
2023-04-14 19:55:59 +0800

    我的界面

---
+++ src_project/modules/mine/MineStore.ts
@@ -0,0 +1,67 @@
+import { action, observable, flow } from "mobx";
+import { request } from "../../utils/Request"
+import Loading from "../../components/widget/Loading";
+
+export default class MineStore {
+
+    @observable refreshing: boolean = false;
+
+    @observable noteList: ArticleSimple[] = [];
+
+    @observable collectionList: ArticleSimple[] = [];
+
+    @observable favorateList: ArticleSimple[] = [];
+
+    @observable info: any = {};
+
+    //使用Promise.all同时请求
+    requestAll = async () => {
+        Loading.show();
+        this.refreshing = true;
+        Promise.all([
+            this.requestNoteList(),
+            this.requestCollectionList(),
+            this.requestFavorateList(),
+            this.requestInfo(),
+        ]).then(() => {
+            Loading.hide();
+            this.refreshing = false;
+        });
+    }
+
+    requestNoteList = async () => {
+        try {
+            const { data } = await request('noteList', {});
+            this.noteList = data || [];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    requestCollectionList = async () => {
+        try {
+            const { data } = await request('collectionList', {});
+            this.collectionList = data || [];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    requestFavorateList = async () => {
+        try {
+            const { data } = await request('favorateList', {});
+            this.favorateList = data || [];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    requestInfo = async () => {
+        try {
+            const { data } = await request('accountInfo', {});
+            this.info = data || {};
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
